a2726179b663c5608ed07c65f5d29a7325a9f224
Divyesh-Sar2003 <119989934+Divyesh-Sar2003@users.noreply.github.com>
2023-09-15 20:09:52 +0530

    create todo list with dag and drop

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React,{useState,useCallback} from 'react'
+import './App.css';
+import Input from './component/Input.js'
+import Draganddrop from './component/Draganddrop.js'
+import { useDropzone } from "react-dropzone";
+
+function App() {
+
+  const getlistfromlocal = localStorage.getItem('todo');
+  const listofarray = JSON.parse(getlistfromlocal);
+  // setListTodo([...listTodo,listofarray]);
+  const [listTodo,setListTodo] = useState(listofarray,[]);
+  let addList = (inputText)=>{
+    if(!inputText){
+      alert('please add some task');
+    }
+    else{
+      const index =  {id: new Date().getTime().toString()}
+      const task ={
+          id:index.id,
+          value:inputText,
+          status:"todo"
+      }
+
+
+      setListTodo([...listTodo,task]);
+      listTodo.push(task);
+    }
+      localStorage.setItem('todo',JSON.stringify(listTodo));
+  }
+  const list = localStorage.getItem('todo');
+  const listedArray = JSON.parse(list);
+  // setListTodo([...listTodo,listedArray]);
+  console.log("listed array ",listedArray);
+  // console.log(setListTodo)/
+  console.log("list todo",listTodo);
+ const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return(
+    <>
+     <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+
+    </>
+
+    );
+};
+export default App;
+
+                  
+    // //  <div className="Container">
+    //     <Input addList={addList} />
+
+    //    <div className='container'>
+    //   <div className="row">
+    //         <div className='col-4 text-center' id='todoCol'>
+    //           <div className='row'>
+    //               <h2 className='table_head_todo'>TO DO</h2>
+    //             </div>
+    //                {listTodo.map((listitem,i)=>{
+    //                 return(
+    //                   <Draganddrop key={i} index={i}
+    //                   item={listitem} 
+    //                   />
+    //                   )
+    //               })}
+    //         </div>
+    //         <div className='col-4 text-center' id='inprogressCol'>
+    //           <div className='row'>
+    //             <h2 className='table_head_inprogress'>IN PROGRESS</h2>
+    //             </div>
+    //               <div className='task_card'>
+    //                 <h3>in progress</h3>
+    //               </div>
+    //         </div>
+    //         <div className='col-4 text-center' id='doneCol'>
+    //           <div className='row'>
+    //             <h2 className='table_head_done'>DONE</h2>
+    //             </div>
+    //               <div className='task_card'>
+    //                 <h3>Done task</h3>
+    //               </div>
+    //         </div>
+    //       </div>
+    // </div>
+    //   </div>
---
+++ src/component/Input.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react'
+
+function Input(props) {
+  const [inputText,setInputText] = useState('');
+  const   handelEnterPress = (e)=>{
+    if(e.keyCode===13){
+      props.addList(inputText)
+      setInputText("");
+    }
+  }
+	return (
+	
+			<div className="taskInput">
+				<input type="text" placeholder="Add your task" 
+					value={inputText}
+					onChange={e =>{setInputText(e.target.value);}} 
+	            	onKeyUp={handelEnterPress} />
+				<button className='addtask'
+					onClick={()=>{props.addList(inputText)
+					setInputText("")}}
+	        	>Add Task</button>
+			</div>
+		
+	);
+}
+
+export default Input

15c635d41cb85875ab936bf67763a9328d43c1d9
Divyesh-sarvaiya-rao <divyeshsarvaiya.rao@gmail.com>
2023-09-18 20:21:40 +0530

    Task drag completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React,{useState,useCallback} from 'react'
+import React,{useState} from 'react'
 import './App.css';
 import Input from './component/Input.js'
-import Draganddrop from './component/Draganddrop.js'
-import { useDropzone } from "react-dropzone";
+// import Draganddrop from './component/Draganddrop.js'
+//import { useDropzone } from "react-dropzone";
+
 
 function App() {
 
   const getlistfromlocal = localStorage.getItem('todo');
   const listofarray = JSON.parse(getlistfromlocal);
   // setListTodo([...listTodo,listofarray]);
-  const [listTodo,setListTodo] = useState(listofarray,[]);
+  // if(listofarray=null){
+  //   const [listTodo,setListTodo] = useState(listofarray);
+  // }
+  const [listTodo,setListTodo] = useState([],listofarray);
   let addList = (inputText)=>{
     if(!inputText){
       alert('please add some task');
@@ -18,8 +22,8 @@ function App() {
       const index =  {id: new Date().getTime().toString()}
       const task ={
           id:index.id,
-          value:inputText,
-          status:"todo"
+          name:inputText,
+          category:"todo"
       }
       setListTodo([...listTodo,task]);
       listTodo.push(task);
@@ -32,63 +36,71 @@ function App() {
   console.log("listed array ",listedArray);
   // console.log(setListTodo)/
   console.log("list todo",listTodo);
- //const onDrop = useCallback(acceptedFiles => {
-    // Do something with the files
-  //}, [])
-  // const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    const onDragOver = (ev) => {
+        ev.preventDefault();
+    }
+    const onDrop = (ev, cat) => {
+       let id = ev.dataTransfer.getData("id");
+       
+       let tasks = listTodo.filter((tasks) => {
+           if (tasks.name === id) {
+               tasks.category = cat;
+           }
+           return tasks;
+       });
+
+      setListTodo([...listTodo,tasks]);
+    }
+    const  onDragStart = (ev, id) => {
+        console.log('dragstart:',id);
+        ev.dataTransfer.setData("id", id);
+    }
+   const tasks = {
+            todo: [],
+            inprogress:[],
+            done: [],
+        }
+   listTodo.forEach ((t) => {
+    tasks[t.category].push(
+        <div key={t.name} 
+            onDragStart = {(e) => onDragStart(e, t.name)}
+            draggable
+            className="draggable"
+            >
+            {t.name}
+        </div>
+    );
+    });
 
   return(
     <>
                      
-     <div className="Container">
-        <Input addList={addList} />
-
-    <div className='container'>
-       <div className="row">
-    <div className='col-4 text-center' id='todoCol'>
-               <div className='row'>
-                   <h2 className='table_head_todo'>TO DO</h2>
+    <div className="container-drag">
+                    <div className='taskInput'>
+                    <Input addList={addList}/>
+                    </div>
+                <div className="todo"
+                      onDrop={(e)=>{onDrop(e, "todo")}}>
+                    <span className="task-header">TO-DO LIST</span>
+                    {tasks.todo}
                 </div>
-                  {listTodo.map((listitem,i)=>{
-                     return(
-                       <Draganddrop key={i} index={i}
-                       item={listitem} 
-                       />
-                       )
-                   })}
+                  <div className="droppable" id='inprogresscol'
+                    onDrop={(e)=>onDrop(e, "inprogress")}
+                    onDragOver={(ev)=>onDragOver(ev)}>
+                    <span className="task-header">IN PROGRESS</span>
+                    {tasks.inprogress}
+                </div>
+               <div className='droppable' id='doneCol'
+                    onDrop={(e)=>onDrop(e, "done")}
+                    onDragOver={(ev)=>onDragOver(ev)}>
+                    <span className="task-header">DONE</span>
+                     {tasks.done}
+
+                </div>
             </div>
-             <div className='col-4 text-center' id='inprogressCol'>
-               <div className='row'>
-               <h2 className='table_head_inprogress'>IN PROGRESS</h2>
-               </div>
-                   <div className='task_card'>
-                     <h3>in progress</h3>
-                   </div>
-             </div>
-             <div className='col-4 text-center' id='doneCol'>
-               <div className='row'>
-                 <h2 className='table_head_done'>DONE</h2>
-                 </div>
-                   <div className='task_card'>
-                     <h3>Done task</h3>
-                   </div>
-             </div>
-           </div>
-     </div>
-       </div>
 
     </>
 
     );
 };
 export default App;
-
-///<div {...getRootProps()}>
-     // <input {...getInputProps()} />
-     // {
-     //   isDragActive ?
-     //     <p>Drop the files here ...</p> :
-        //  <p>Drag 'n' drop some files here, or click to select files</p>
-     // }
-    //</div>
-  
